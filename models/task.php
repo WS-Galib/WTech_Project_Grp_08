8736b2ff63e9625f707299963999b28519eb7d90
Borno <ornob.2202@gmail.com>
2026-05-15 21:29:13 +0600

    getTaskByStatus function created in db

--- models/task.php
+++ models/task.php
@@ -7,4 +7,11 @@ class task
         $result = $connection->query($sql);
         return $result;
     }
+
+    function getTasksByStatus($connection, $tablename, $project_id, $status)
+    {
+        $sql = "SELECT * FROM " . $tablename . " WHERE project_id='" . $project_id . "' AND status='" . $status . "'";
+        $result = $connection->query($sql);
+        return $result;
+    }
 }
