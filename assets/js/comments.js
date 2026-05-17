8452975c76f3e928d2d2824371e399b3109def60
Showrob098 <Showrobahmad98@gmail.com>
2026-05-17 21:14:23 +0600

    task redirect when click the move button

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -3,3 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (boardContainer) {
         boardContainer.addEventListener("click", function (event) {
+                        if (event.target.classList.contains("move-btn")) {
+                return; // Let Student 3's code handle the column movement
+            }
+
